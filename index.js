4d678e6a21cfc051e1987b9815f4cf67f63b83f4
AHenry95 <ahenry.dev@gmail.com>
2025-05-10 11:41:14 -0400

    Update contents of application pages
    - updated index.html, documentation.html, and index.js's welcome message to reflect changes made to the application

--- index.js
+++ index.js
@@ -51,8 +51,12 @@ app.use(morgan('common'));
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.send('Welcome to the myFlix app!');
+    res.send('Welcome to the myFlix app! Please <a href="/index.html">Click Here </a> to navigate to the home page!');
 });
+
+app.get('/index.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+})
 
 app.get('/movies', (req, res) =>{
     res.json(topMovies);
