e62b85d9d10c913083e75c9f3803d147dc1280d4
AHenry95 <ahenry.dev@gmail.com>
2025-06-28 16:51:08 -0400

    Connected App with live API on Atlas

--- index.js
+++ index.js
@@ -25,7 +25,11 @@ app.use(express.static('public'));
 
 let auth = require('./auth')(app);
 
-mongoose.connect('mongodb://localhost:27017/myFlixDB', {useNewUrlParser: true, useUnifiedTopology: true});
+// Connects with local MongoDB database
+// mongoose.connect('mongodb://localhost:27017/myFlixDB', {useNewUrlParser: true, useUnifiedTopology: true});
+
+// Connects with Atlas databse
+mongoose.connect('process.env.CONNECTION_URI', {useNewUrlParser: true, useUnifiedTopology: true});
 
 //CREATE Requests 
 
