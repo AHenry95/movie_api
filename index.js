9f6221b04822cc6f22f0089ee3ba9dd6ac78d945
AHenry95 <ahenry.dev@gmail.com>
2025-07-09 19:15:55 -0400

    Re-establish connection with Atlas db

--- index.js
+++ index.js
@@ -26,10 +26,10 @@ app.use(express.static('public'));
 let auth = require('./auth')(app);
 
 // Connects with local MongoDB database
-mongoose.connect('mongodb://localhost:27017/myFlixDB', {useNewUrlParser: true, useUnifiedTopology: true});
+// mongoose.connect('mongodb://localhost:27017/myFlixDB', {useNewUrlParser: true, useUnifiedTopology: true});
 
 // Connects with Atlas databse
-// mongoose.connect(process.env.CONNECTION_URI, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.CONNECTION_URI, {useNewUrlParser: true, useUnifiedTopology: true});
 
 //CREATE Requests 
 
